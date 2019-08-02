dd0f931170a4fed9b8e5b70de4a6c6e1d60bba60
amrkh97 <amrkh97@gmail.com>
2019-08-02 11:00:32 +0200

    Added Models for Sending BatchID and VIN

---
+++ src/main/java/Models/AmbBatchMapModel.java
@@ -0,0 +1,20 @@
+package Models;
+
+public class AmbBatchMapModel {
+
+	private Long batchID;
+	private Integer vin;
+	
+	public Long getBatchID() {
+		return batchID;
+	}
+	public void setBatchID(Long batchID) {
+		this.batchID = batchID;
+	}
+	public Integer getVin() {
+		return vin;
+	}
+	public void setVin(Integer vin) {
+		this.vin = vin;
+	}
+}
